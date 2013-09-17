146a2de4eccd96b6c49e685e4948395305d6c8d3
Adam Beagle <abeagle88@gmail.com>
2013-09-17 14:18:52 -0400

    Update: config.py
    Added resolution notes.

--- src/jeoparpy/config.py
+++ src/jeoparpy/config.py
@@ -16,5 +16,11 @@ of source code from this file.
 """
 
 FPS_LIMIT = 100
-FULLSCREEN = 0
-SCREEN_W, SCREEN_H = SCREEN_SIZE = (1280, 720)#(800, 450)#(1280, 720)
+FULLSCREEN = 1
+
+# Reccomended common 16:9 resolutions:
+# (640, 360), (854, 480), (960, 540), (1024, 576),
+# (1280, 720), (1366, 768), (1600, 900), (1920, 1080)
+#
+# It is not recommended to use a resolution higher than (1920, 1080).
+SCREEN_W, SCREEN_H = SCREEN_SIZE = (1366, 768)
