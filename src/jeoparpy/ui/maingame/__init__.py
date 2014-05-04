3c36ddc8511b3e6ca70c836157a248899225f7ce
Adam Beagle <abeagle88@gmail.com>
2014-05-04 01:54:11 -0400

    Add docstring for jeoparpy.ui.maingame

--- src/jeoparpy/ui/maingame/__init__.py
+++ src/jeoparpy/ui/maingame/__init__.py
@@ -1,3 +1,9 @@
+"""
+This package (jeoparpy.ui.maingame) contains the ui modules involved in the
+primary gameplay of JeoparPy, i.e. everything from when the main game board 
+and podia appear until the game is over and the congratulations screen 
+appears.
+"""
 from clue import Clue
 from clueanimation import OpenClueAnimation
 from gameboard import GameBoard
